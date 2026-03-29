fe5dc38f83266dc15d017ee195fd73f62eea41a7
Emre <sg2golge@gmail.com>
2026-03-29 21:33:54 +0300

    Pass Finito

--- Sene Odevi Fronted/js/passUpdate.js
+++ Sene Odevi Fronted/js/passUpdate.js
@@ -2,5 +2,25 @@ function Update() {
     const username = document.getElementById("username").value.trim();
     const newPass = document.getElementById("newPass").value.trim();
     const newPassRepeat = document.getElementById("newPassRepeat").value.trim();
-    fetch()
+        fetch("https://localhost:7074/updatepass", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+             username:Username,
+                newPass:Pass,
+                newPassRepeat:PassRepeat
+
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            alert(data.message)
+            if (data.success) {   // backend tarafında success = true dönüyorsa
+                window.location.href = "giriş.html"; // Login e git 
+            }
+        })
+        .catch(err => {
+            //Hata Olur ise consola yazdır 
+            console.error("Fetch hatası:", err);
+        });
 }
