39f04ea5cbd398df223bb1acfaac72a9f7fe12de
Emre <sg2golge@gmail.com>
2026-04-08 20:57:17 +0300

    Pass Update Modal-1

--- Sene Odevi Fronted/js/modal.js
+++ Sene Odevi Fronted/js/modal.js
@@ -24,28 +24,3 @@ document.addEventListener("keydown", function (e) {
         closeModal();
     }
 });
-
-// ======================
-// ŞİFRE SIFIRLAMA
-// ======================
-function sendCode() {
-    const user = document.getElementById("resetUser").value.trim();
-
-    if (!user) {
-        alert("Kullanıcı adı gir");
-        return;
-    }
-
-    fetch("http://localhost:7074/send-code", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ Username: user })
-    })
-        .then(res => res.json())
-        .then(data => {
-            alert(data?.message || "Kod gönderildi");
-        })
-        .catch(() => {
-            alert("Kod gönderilemedi!");
-        });
-}
